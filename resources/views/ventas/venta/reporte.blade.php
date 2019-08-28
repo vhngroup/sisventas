65c114824f0e2f137d2e0a2f1788dc851abe54f6
vhngroup <info@vhngroup.com>
2019-08-27 22:41:44 -0500

    Cambio de numeraciòn, en el paginado de facturaciòn.

--- resources/views/ventas/venta/reporte.blade.php
+++ resources/views/ventas/venta/reporte.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-    <title>Factura de Venta # {{$venta->idventa}}</title>
+    <title>Factura de Venta # {{$venta->num_comprobante}}</title>
     <link rel="stylesheet" href="{{asset('css/style.css')}}" media="all" />
    </head>
   <body>
