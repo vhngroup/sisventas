817e5e4a73ecaba4f231b53b9b294f5010e39995
vhngroup <info@vhngroup.com>
2019-09-30 01:50:52 -0500

    Corecciòn de linea de codigo asociada a la busqueda de articulo en el modulo de cotizaciones

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -25,20 +25,19 @@ class CotizacionController extends Controller
     {
     	if($request)
     	{
-    		$query=trim($request->GET('searchText'));
-
+    	$query=trim($request->GET('searchText'));
         $cotizacion=DB::table('cotizacion as c')
          ->join('persona as p','c.idcliente','=','p.idpersona')
          ->join('detallecotizacion as dc','c.idcotizacion','=','dc.idcotizacion')
-         ->join('articulo as ac', 'dc.idarticulo', '=' , 'ac.idarticulo')
-    		->select('c.idcotizacion','c.fecha_hora','p.nombre','c.serie_comprobante','c.num_comprobante','c.descripcion','c.estado','c.total_venta','c.condiciones')
+         ->join('articulo as ac', 'dc.idarticulo','=','ac.idarticulo')
+    	 ->select('c.idcotizacion','c.fecha_hora','p.nombre','c.serie_comprobante','c.num_comprobante','c.descripcion','c.estado','c.total_venta','c.condiciones')
     		->where('c.num_comprobante','LIKE','%'.$query.'%')
     		->orwhere('c.descripcion','LIKE','%'.$query.'%')
-            ->orwhere('p.nombre','LIKE','%'.$query.'%')
             ->orwhere('ac.codigo','LIKE','%'.$query.'%')
+            ->orwhere('p.nombre','LIKE','%'.$query.'%')
             ->orderBy('c.idcotizacion','desc')
     		->groupBy('c.idcotizacion','c.fecha_hora','p.nombre','c.serie_comprobante','c.num_comprobante','c.estado')
-    		->paginate(10);
+    		->paginate(15);
     		return view('cotizaciones.index',["cotizacion"=>$cotizacion,"searchText"=>$query]);
     	}
     }
